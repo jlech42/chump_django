fb69d9b709b04e26bddf96c45e090580459b3791
Justin Lechner <jlech42@gmail.com>
2017-07-13 10:42:24 -0700

    bug fix again

--- api/views.py
+++ api/views.py
@@ -199,53 +199,18 @@ def GetContentBlocksFromTags(request):
     parsed_response = json.loads(req_body)
     elements = get_elements(parsed_response)
     chatfuel_response = {
- "messages": [
-    {
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type":"generic",
-          "elements":[
+        "messages": [
             {
-              "title":"Classic White T-Shirt",
-              "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-              "subtitle":"Soft white cotton t-shirt is back in style",
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-                  "title":"View Item"
-                },
-                {
-                  "type":"web_url",
-                  "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-                  "title":"Buy Item"
+                "attachment":{
+                    "type":"template",
+                    "payload":{
+                        "template_type":"generic",
+                        "elements":elements
+                    }
                 }
-              ]
-            },
-            {
-              "title":"Classic Grey T-Shirt",
-              "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
-              "subtitle":"Soft gray cotton t-shirt is back in style",
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
-                  "title":"View Item"
-                },
-                {
-                  "type":"web_url",
-                  "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
-                  "title":"Buy Item"
-                }
-              ]
             }
-          ]
-        }
-      }
+        ]
     }
-  ]
-}
     print(chatfuel_response)
     return JsonResponse(chatfuel_response)
 
