fdf45ee8e149227752de6cc053eb5f5c43ad208d
Justin Lechner <jlech42@gmail.com>
2017-07-13 18:45:20 -0700

    updated to handle one element

--- api/views.py
+++ api/views.py
@@ -160,8 +160,11 @@ def get_elements(parsed_response):
             i = i + 1
         else:
             break
-
-    return elements
+    if len(elements) == 1:
+        elements = elements [0]
+        return elements
+    else:
+        return elements
 
 def GetSubscriptionFromMessengerID(id):
     user_id = User.objects.get(username=id).id
