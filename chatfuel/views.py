ea7c6beea67a9ed81117f92990ed3c6d9d860ee3
Justin Lechner <jlech42@gmail.com>
2017-07-27 14:54:19 -0700

    fix

--- chatfuel/views.py
+++ chatfuel/views.py
@@ -58,7 +58,24 @@ def ContentLearnMoreMessageResponse(request):
                   ]
             }
           }
+        },
+        {
+            "text":  "You can also use the below options",
+      "quick_replies": [
+        {
+            "title":"Keep exploring",
+            "block_names":["explore_content"]
+        },
+        {
+            "title":"See watchlist",
+            "block_names":["watchlist"]
+        },
+        {
+            "title":"Share w/ friends",
+            "block_names":["Share"]
         }
+      ]
+    }
       ]
     }
     '''
