1402601bf28623af9aa9cb4f3161af8bffd71dab
Justin Lechner <jlech42@gmail.com>
2017-07-29 16:22:21 -0700

    fix

--- user/views.py
+++ user/views.py
@@ -41,7 +41,9 @@ FB_USER_API = FB_URL_ROOT+FB_URL_PARAMS
 def facebook_webhooks(request):
     print('fb webhook')
     body = request.GET
-    print(body)
+    post_body = request.POST
+    print('body', body)
+    print('post body', post_body)
     if (body.get('hub.mode') == 'subscribe') & (body.get('hub.verify_token') =='test_token'):
         print('verified!!')
         challenge_response = body.get('hub.challenge')
